fdec3ef089c97f03bd67a2b13a972a2785d9f339
Andreas Vifert <andreasgylche@gmail.com>
2022-07-29 22:42:41 +0200

    added navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,38 @@
-import styles from "../styles/Navbar.module.css";
+import styles from '../styles/Navbar.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
-  return <p>Navbar</p>;
+  const router = useRouter();
+
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a className={styles.logo}>CryptoTrackr</a>
+      </Link>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link href="/">
+              <a className={router.asPath == '/' ? styles.active : ''}>Home</a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/coins">
+              <a className={router.asPath == '/coins' ? styles.active : ''}>
+                Coins
+              </a>
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/markets">
+              <a className={router.asPath == '/markets' ? styles.active : ''}>
+                Markets
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
